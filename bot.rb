bf0610d9680db0766024954499fa572da86c43f3
Ivan Gonzalez <ivan@simplelogica.net>
2016-03-16 01:46:03 +0100

    Added quit option.

--- bot.rb
+++ bot.rb
@@ -8,9 +8,12 @@ brains = Dir.glob("sara_spanish_aiml/*")
 robot = ProgramR::Facade.new
 robot.learn(brains)
 
-while true
+begin
   print '>> '
   s = STDIN.gets.chomp
-  reaction = robot.get_reaction(s)
-  STDOUT.puts "<< #{reaction}"
-end
+  
+  if s != 'quit'
+    reaction = robot.get_reaction(s)
+    STDOUT.puts "<< #{reaction}"
+  end
+end while s != 'quit'
